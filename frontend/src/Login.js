9d8e8384cbf15e93154f2e9fafd3f0c78aa9bed5
elin620 <43224346+elin620@users.noreply.github.com>
2020-02-29 00:49:53 -0700

    Add files via upload

---
+++ frontend/src/Login.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="Login">
+      <header className="Login-header">
+      </header>
+      <div className="Prompt-box">
+        <h2 className="Prompt-header">Secure Account Log In <i class="material-icons"> lock</i></h2>
+        <p className="Prompt-textinput">
+          <label for="userID">&nbsp;&nbsp;&nbsp;User ID&nbsp;&nbsp;</label>
+          <input type="text" id="userID" name="userID"/>
+        </p>
+        <p className="Prompt-textinput">
+          <label for="password">Password&nbsp;&nbsp;</label>
+          <input type="text" id="password" name="password"/>
+        </p>
+          
+          
+        <p className="Login-link">
+          <a
+            href="https://reactjs.org"
+            rel="noopener noreferrer"
+          >
+            Forgot user ID/password?
+          </a>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <a
+            href="https://reactjs.org"
+            rel="noopener noreferrer"
+          >
+            Create account
+          </a>
+        </p>
+        <input type="submit" value="Sign In"/>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
