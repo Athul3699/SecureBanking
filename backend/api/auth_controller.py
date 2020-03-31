48be3bf7ca0710dbb71b0fd576030abbb7de6c66
ronaktanna <ronaktanna.personal@gmail.com>
2020-03-31 02:22:30 -0700

    modified authentication

--- backend/api/auth_controller.py
+++ backend/api/auth_controller.py
@@ -1,21 +1,34 @@
-from flask import jsonify, g, Blueprint, request
+from flask import jsonify, g, Blueprint, request, make_response
 from backend import app
 from backend.services import auth_service
+from backend.services import auth_service2
 
 auth_api = Blueprint('auth_api', __name__)
 
 
-@auth_api.route("/LoginUser", methods=['POST'])
-def login():
-    app.logger.info("[api-login-user]")
-    args = request.json
+@auth_api.route("/RegisterUser", methods=['POST'])
+def register_user_api():
+    data = request.json
 
-    email = args['email']
-    password = args['password']
+    status, data = auth_service2.register_user(**data)
 
-    response = auth_service.login_user(email=email, password=password)
-
-    if response["status"] == "success":
-        return jsonify(response)
+    if status == "success":
+        return make_response(jsonify({ "status": status, "data": data })), 200
+    elif status == "failure" and data == "user already exists":
+        return make_response(jsonify({ "status": status, "data": data})), 304
     else:
-        return jsonify(response)
+        return make_response(jsonify({ "status": status, "data": data})), 500
+
+
+@auth_api.route("/LoginUser", methods=['POST'])
+def login_user_api():
+    data = request.json
+
+    status, data = auth_service2.login_user(**data)
+
+    if status == "success":
+        return make_response(jsonify({ "status": status, "data": data })), 200
+    elif status == "failure" and data == "user does not exist":
+        return make_response(jsonify({ "status": status, "data": data})), 304
+    else:
+        return make_response(jsonify({ "status": status, "data": data})), 500
--- backend/model/manage.py
+++ backend/model/manage.py
@@ -5,9 +5,15 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 import datetime
 from sqlalchemy.types import JSON
+import jwt
+
+import sys
+sys.path.append("..") # Adds higher directory to python modules path.
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:t@localhost:5432/securebank'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///securebank'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -66,7 +72,68 @@ class User(db.Model):
     edit_data = db.Column(JSON, default={})
     edit_mode = db.Column(db.Boolean, default=False)
     is_active = db.Column(db.Boolean, default=True)
+    activeJWT = db.Column(db.String, default='')
 
+    def encode_auth_token(self, email):
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=1200),
+                'email': email
+            }
+            
+            return jwt.encode(
+                payload,
+                "justatest",
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Validates the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, "justatest")
+            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
+            if is_blacklisted_token:
+                raise ValueError('Token blacklisted. Please log in again.')
+            else:
+                return payload['email']
+        except jwt.ExpiredSignatureError:
+            raise ValueError('Signature expired. Please log in again.')
+        except jwt.InvalidTokenError:
+            raise ValueError('Invalid token. Please log in again.')
+
+
+class BlacklistToken(db.Model):
+    """
+    Token Model for storing JWT tokens
+    """
+    __tablename__ = 'blacklist_tokens'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+    blacklisted_on = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, token):
+        self.token = token
+        self.blacklisted_on = datetime.datetime.now()
+
+    def __repr__(self):
+        return '<id: token: {}'.format(self.token)
+
+    @staticmethod
+    def check_blacklist(auth_token):
+        # check whether auth token has been blacklisted
+        res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
+        if res:
+            return True
+        else:
+            return False
 
 
 class Bankaccount(db.Model):
