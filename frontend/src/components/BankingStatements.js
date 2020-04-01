f7ada41ed5807739998fbc7e9262caaf626e1ec2
surya23 <surya.cherukuri@admitkard.com>
2020-04-01 00:35:53 -0700

    Edited Banking Statements

--- frontend/src/components/BankingStatements.js
+++ frontend/src/components/BankingStatements.js
@@ -2,20 +2,43 @@ import React, { Component } from "react";
 import MonthYearPicker from 'react-month-year-picker';
 import { postRequest } from '../util/api';
 import { API_URL } from '../constants/references';
+import { Menu, Dropdown, DatePicker, Button} from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 class BankingStatements extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      month: 4,
-      year: 2020,
-      statements: []
+      account_number: [],
+      month: 0,
+      year: 0,
+      statements: [],
+      accountId: null
     };
 
-    this.handleClick = this.handleClick.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onButtonClick = this.onButtonClick.bind(this);
   }
 
-  handleClick(event){
-    postRequest(`${API_URL}/api/v1/auth/LoginUser`, { "month": this.state.month, "password": this.state.year })
+  // componentDidMount() {
+  //   console.log('GrandChild did mount.');
+  //   postRequest(`${API_URL}/user/GetBankAccounts`)
+  //   .then((data) => {
+  //     console.log(data);
+  //     window.localStorage.setItem('API_TOKEN', data["data"]["token"])
+  //   })
+  //   .catch((error) => console.log(error))
+  // }
+
+  onChange(date, dateString) {
+    var dateArray = dateString.split("-");
+    console.log(parseInt(dateArray[0]));
+    console.log(parseInt(dateArray[1]));
+    this.setState({month:parseInt(dateArray[1]), year:parseInt(dateArray[0])});
+ 
+  }
+
+  onButtonClick(event){
+    postRequest(`${API_URL}/api/v1/transaction/DownloadStatements`, { "month": this.state.month, "password": this.state.year, "account_number": this.state.account_number })
     .then((data) => {
       //set state for statements
       window.localStorage.setItem('API_TOKEN', data["data"]["token"])
@@ -24,31 +47,32 @@ class BankingStatements extends Component {
   }
 
   render() {
-    var buttonStyle = {
-      margin: '10px 10px 10px 0'
+    const onClick = ({ key }) => {
+      console.log(key);
     };
-    const list = ['a', 'b', 'c'];
+
+    const menu = (
+      <Menu onClick={onClick}>
+        <Menu.Item key="1">1st menu item</Menu.Item>
+        <Menu.Item key="2">2nd memu item</Menu.Item>
+        <Menu.Item key="3">3rd menu item</Menu.Item>
+      </Menu>
+    );
     return (
       <div>
-        <MonthYearPicker
-          selectedMonth={this.state.month}
-          selectedYear={this.state.year}
-          minYear={2000}
-          maxYear={2030}
-          onChangeYear={year => this.setState({ year: year })}
-          onChangeMonth={month => this.setState({ month: month })}
-        />
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <button
-        className="btn btn-default"
-        style={buttonStyle}
-        onClick={this.handleClick}>Get Statements
-      </button>
-      {/* <ul>
-        {this.state.statements.map(item => (
-          <li key={item}>{item}&nbsp;&nbsp;&nbsp;&nbsp;<a><img src="./download.png" alt="download"/></a></li>
-        ))}
-      </ul> */}
+        <Dropdown overlay={menu}>
+          <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+            Select Account <DownOutlined />
+          </a>
+        </Dropdown>
+        <br />
+        <br />
+        <br />
+        <DatePicker onChange={this.onChange} picker="month" />
+        <br />
+        <br />
+        <br />
+        <Button type="primary" onClick={this.onButtonClick}>Download Statements</Button>
       </div>
     );
   }
