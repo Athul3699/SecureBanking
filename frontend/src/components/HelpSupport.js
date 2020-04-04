8032053a7bcef5f7beba6c91af4b1b6986155af6
elin620 <43224346+elin620@users.noreply.github.com>
2020-04-04 14:45:46 -0700

    ui update

--- frontend/src/components/HelpSupport.js
+++ frontend/src/components/HelpSupport.js
@@ -1,50 +1,92 @@
 import React, { Component } from "react";
 
-
+import "antd/dist/antd.css";
 import './HelpSupport.css';
 
+import { Input, Button, Radio, Select, InputNumber } from "antd";
+import { postRequest, getRequest } from "../util/api";
+import { API_URL } from "../constants/references";
+import { withRouter } from "react-router-dom"
 
+const { TextArea } = Input;
+const { Option } = Select;
 
 class HelpSupport extends Component {
   constructor(props) {
     super(props);
     this.state = {
 
-      feedback:'',
-      accType:'',
-      startDate: new Date()
+      description:''
     };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleChange(event){
-
-    this.setState({ [event.target.name]: event.target.value });
-    console.log(event.target.name); // the name of the form element
-    console.log(event.target.value); // the value of the form element
+  componentDidMount() {
+    this.refreshAccountState()
   }
+  refreshAccountState = () => {
+    //getRequest(`${API_URL}/api/v1/bank_account/GetActiveCustomerAccounts`)
+    //.then((data) => {
+     // console.log(data["data"])
+      //if (data["data"].length > 0) {
+        //this.setState({ accounts: data["data"].map( (account, i) => {
+         // return {
 
-  handleSubmit(event) {
-    
-
+         // }
+       // })})
+     // }
+ //   })
+ //   .catch ((err) => console.log(err))
   }
+  setComponentSize = () => {
+    this.setState({ componentSize: "small" });
+  };
+  
+  handleDescriptionChange = e => {
+    this.setState({ description: e.target.value });
+  };
+  
+  validate = () => {
+   
+
+    return true;
+  };
+
+  onButtonClick = () => {
+    if (this.validate()) {
+      const body = {
+        "description": this.state.description
+
+      }
+
+      postRequest(`${API_URL}/api/v1/transaction/InitiateMoneyTransfer`, body)
+        .then(() => this.props.history.push('/manageRequests'))
+        .catch(() => {});
+    } else {
+      alert("Invalid values in the form!!");
+    }
+  };
+
   render() {
   return ( 
-    <div className='mng'>  
-      <div className='hs'>  
-      <p className="hs-header">Help &amp; Support</p>
-      <label>Feedback/Report Problem&nbsp;&nbsp;</label>
-      <form onSubmit={this.handleSubmit}>
-        <textarea  style={{ resize: "none", height: "120px" }} name="feedback" value={this.state.feedback} onChange={this.handleChange} />
-        <div className="hs-button"><input type="submit" value="Submit" /></div>
-      </form>
-        
 
-</div>  
-</div>  
+      <div className="create-form-container">
+      <h4>Help &amp; Support</h4>
+      <br />
+        <br />
+        Feedback/Report Problem:
+        <TextArea
+          rows={4}
+          value={this.state.description}
+          allowClear
+          onChange={this.handleDescriptionChange}
+        />
+        <br />
+        <br />
+        <Button type="primary" onClick={() => this.onButtonClick()}>
+          Submit Request
+        </Button>
+      </div>
   );
   }
 }
+export default withRouter(HelpSupport);
 
-export default HelpSupport;
